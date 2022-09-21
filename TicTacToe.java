04b54c2c490e66cd4b39fded4b7c9d3eb92a7942
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-21 09:39:02 +0530

    ADD:CHOOSING A LETTER OF X OR O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,34 @@
 package GAME.game;
 
-public class TicTacToe 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Scanner;
+
+public class TicTacToe{
+	
+	static char[] board = new char[10];
+	public static void board() {
+		for(int i=1; i<10; i++) {
+			board[i]=' ';
+		}
+	}
+	
+	static void letter(char i){
+		if(i=='X') {
+			System.out.println("PLAYER CHOSSED :" +i);
+		}
+		else
+			System.out.println("PLAYER CHOSSED:O ");
+		
+	  }
+	public static void main(String[] args) {
+	System.out.println("WELCOME TO TIC TAE TOE");
+	board();
+	Scanner sc = new Scanner(System.in);
+    System.out.println("Enter you choice wheather X OR Y");
+    char a = sc.next().charAt(0); 
+    letter(a);
+    
+	}
+
+		
+	
 }
